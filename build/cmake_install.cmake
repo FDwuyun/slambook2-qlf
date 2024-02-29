e72a7fef26b1eda00340ac5a5ef093a53a69a612
QLF <2462750467@qq.com>
2024-02-29 14:05:48 +0800

    update .gitignore

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -38,9 +38,8 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
+  # Include the install script for the subdirectory.
   include("/qlf_home/Documents/slambook2-qlf/build/ch6/cmake_install.cmake")
-
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
